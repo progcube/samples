94916c92f90d6caadf3b81bb33e2d43f2326251a
mleboeuf <ml@maximeleboeuf.com>
2017-10-15 00:01:40 -0400

    Create MVC project for Subscriptions sample

---
+++ Progcube.Subscriptions.Sample/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Progcube.Subscriptions.Sample.Models;
+
+namespace Progcube.Subscriptions.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Customers");
+        }
+
+        public IActionResult Customers()
+        {
+            return View();
+        }
+
+        public IActionResult Plans()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Discounts()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Progcube.Subscriptions.Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Progcube.Subscriptions.Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
